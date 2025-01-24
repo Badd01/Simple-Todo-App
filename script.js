7bd40440cc4ad4d733e9eda88452d413b3ddc6a6
Badd <dang.hoangthe01@gmail.com>
2025-01-24 21:31:34 +0700

    Version 2.0

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ const createTaskBtn = document.querySelector(".create-task");
 const input = document.querySelector("input");
 const list = document.querySelector("ol");
 
-createTaskBtn.addEventListener("click", () => {
+createTaskBtn.addEventListener("click", addTask);
+
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    addTask();
+  }
+});
+
+function addTask() {
   const task = input.value;
   input.value = "";
 
@@ -11,20 +19,43 @@ createTaskBtn.addEventListener("click", () => {
   const deleteBtn = document.createElement("button");
   deleteBtn.textContent = "Delete";
   deleteBtn.classList = "delete-task";
+  const addSubTask = document.createElement("button");
+  addSubTask.textContent = "Add";
+  const todo = document.createElement("div");
+  todo.classList = "todo";
+  const feature = document.createElement("div");
+  feature.classList = "feature";
+
+  const nameTask = document.createElement("h3");
 
   if (task !== "") {
     list.appendChild(listItem);
-    listItem.textContent = task;
-    listItem.appendChild(deleteBtn);
+    listItem.appendChild(todo);
+    todo.appendChild(nameTask);
+    nameTask.textContent = task;
+    todo.appendChild(feature);
+    feature.appendChild(addSubTask);
+    feature.appendChild(deleteBtn);
   }
+
+  addSubTask.addEventListener("click", () => {
+    const subTask = document.createElement("ul");
+    const subItem = document.createElement("li");
+    const inputSub = document.createElement("input");
+    listItem.appendChild(subTask);
+    subTask.appendChild(subItem);
+    subItem.appendChild(inputSub);
+
+    inputSub.addEventListener("keydown", (e) => {
+      if (e.key === "Delete") {
+        subTask.removeChild(subItem);
+      }
+    });
+  });
 
   deleteBtn.addEventListener("click", () => {
     list.removeChild(listItem);
   });
 
-  listItem.addEventListener("click", () => {
-    listItem.classList.toggle("done");
-  });
-
   input.focus();
-});
+}
